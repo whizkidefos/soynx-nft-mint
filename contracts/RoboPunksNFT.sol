23117efcb077a08f62ae783bb19fa1a7795b65de
Efosa Igbinehi <whizkidefos@users.noreply.github.com>
2022-05-18 16:26:03 +0100

    init commit after scaffold

---
+++ contracts/RoboPunksNFT.sol
@@ -0,0 +1,18 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract RoboPunksNFT is ERC721, Ownable {
+    uint256 public mintPrice;
+    uint256 public totalSupply;
+    uint256 public maxSupply;
+    uint256 public maxPerWallet;
+    bool public isPublicMintEnabled;
+    string internal baseTokenUrl;
+    address payable public withdrawWallet;
+    mapping(address => uint256) public walletMints;
+
+    constructor() payable ERC721("RoboPunks", "RP") {}
+}
